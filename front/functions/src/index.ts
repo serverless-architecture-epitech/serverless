be32400b795723da7b01e367baef6bfdbb56a27e
Matteo <matteo.de-faria@epitech.eu>
2022-05-30 10:41:10 +0200

    add: first firebase function

--- front/functions/src/index.ts
+++ front/functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+
+exports.addToGroups = functions.firestore
+  .document("/users/{user}")
+  .onCreate((snap, context) => {
+    const original = snap.data().original;
+
+    functions.logger.log("Data", context.params.user, original);
+  });
+
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
